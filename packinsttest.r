6303d2598d797072fa98d6f9b8b75590acc8676b
Anthokyoo <anthony.son@outlook.fr>
2024-09-19 11:31:04 +0200

    Test shiny en cours

--- packinsttest.r
+++ packinsttest.r
@@ -1,1 +1,4 @@
 install.packages('rsconnect')
+install.packages(c('ggplot2', 'shiny'))
+library(shiny)
+runApp()
--- test.R
+++ test.R
@@ -1,5 +1,5 @@
 ---
-title: "Viridis Demo"
+title: "Test"
 output: html_document
 ---
 
@@ -19,4 +19,15 @@ image(volcano, col = viridis(200))
 
 ```{r}
 image(volcano, col = viridis(200, option = "A"))
+```
+
+## AUTHORIZE ACCOUNT
+```{r}
+rsconnect::setAccountInfo(name='fmrd2e-anthokyoo', token='3AEAAAFBC31A3D9D3304A5EB67EDBFFD', secret='8FbP1YBWOkAYiJfqxRm0PC/CdSz5nSyKgA7GhNGZ')
+```
+
+## DEPLOY
+```{r}
+library(rsconnect)
+    rsconnect::deployApp('path/to/your/app')
 ```
